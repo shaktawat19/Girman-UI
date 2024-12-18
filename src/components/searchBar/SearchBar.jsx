7ce00470732b7803587e5bc172580c6d6a10e497
shaktawat19 <harshshaktawat.tech@gmail.com>
2024-12-18 21:23:16 +0530

    Added search bar component

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./SearchBar.css";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = () => {
+    if (searchTerm.trim()) {
+      navigate(`/search?q=${searchTerm}`);
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSearchSubmit();
+    }
+  };
+
   return (
-    <div>SearchBar</div>
-  )
-}
+    <div
+      className="search-bar-container"
+      style={props?.isSearchBar ? { margin: "1.2rem auto" } : {}}
+    >
+      <input
+        type="text"
+        placeholder="Search"
+        className="search-bar"
+        value={searchTerm}
+        onKeyDown={handleKeyDown}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <i className="fa fa-search search-icon"></i>
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
